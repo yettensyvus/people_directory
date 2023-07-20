47c71d09c3986918c2392d506292eaff62eb2191
Aleksandr Straton <houka1337@gmail.com>
2023-07-20 16:11:48 +0300

    added toString function and tested it

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import org.example.classes.People;
+import org.example.classes.PeopleController;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        PeopleController pc = new PeopleController("src/main/java/org/example/data/people.txt");
+
+        ArrayList<People> people_list;
+
+        people_list = pc.get_people();
+
+        for(int i = 0; i < people_list.size(); i++)
+        {
+            System.out.println(people_list.get(i).toString());
+        }
     }
 }
--- src/main/java/org/example/classes/People.java
+++ src/main/java/org/example/classes/People.java
@@ -1,13 +1,18 @@
 package org.example.classes;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
 public class People {
 
     //people private variables
     private int id_people;
     private String name ;
     private String surname;
-    private String email;
+    private LocalDate date_of_birth;
     private String number;
+    private String email;
 
     //getter of private variables
     public int get_id_people()
@@ -19,55 +24,87 @@ public class People {
     {
         return name;
     }
-
-    public String get_email()
+    public String get_surname()
     {
-        return email;
+        return surname;
+    }
+
+    private LocalDate get_date_of_birth()
+    {
+        return date_of_birth;
     }
 
     public String get_number()
     {
         return number;
     }
+    public String get_email()
+    {
+        return email;
+    }
+
 
     //setter of private variables
     public int set_id_people(int id_people)
     {
-        this.id_people = id_people;
+        return this.id_people = id_people;
     }
 
     public String set_name(String name)
     {
-        this.name = name;
+        return this.name = name;
     }
 
     public String set_surname(String surname)
     {
-        this.surname = name;
+        return this.surname = name;
+    }
+    public LocalDate set_date_of_birth(LocalDate date_of_birth)
+    {
+        return this.date_of_birth = date_of_birth;
+    }
+
+
+
+    public String set_number(String number)
+    {
+        return this.number = number;
     }
 
     public String set_email(String email)
     {
-        this.email = email;
+        return this.email = email;
     }
-
-    public String set_number(String number)
-    {
-        this.number = number;
-    }
-
+    //emplty implicit constructor
     public People()
     {
-        name = surname = email = number =  null;
+        name = surname = number = email =  null;
     }
 
-    public People(int id_people, String name, String surname, String email, String number)
+    //constructor with parameters
+    public People(int id_people, String name, String surname, LocalDate date_of_birth, String number, String email)
     {
         this.id_people = id_people;
         this.name = name;
         this.surname = surname;
-        this.email = email;
+        this.date_of_birth = date_of_birth;
         this.number = number;
+        this.email = email;
     }
 
+    public People(String file_line)
+    {
+        String[] file_data = file_line.split(" ");
+        id_people = Integer.parseInt(file_data[0]);
+        name = file_data[1];
+        surname = file_data[2];
+        date_of_birth = LocalDate.parse(file_data[3]);
+        number = file_data[4];
+        email = file_data[5];
+    }
+
+    public String toString()
+    {
+        return get_id_people() + " " + get_name() + " " + get_surname() + " " + get_date_of_birth().toString() + " " + get_number() + " " + get_email();
+    }
 }
