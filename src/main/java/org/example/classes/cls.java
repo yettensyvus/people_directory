29112a0328423224fec81fbb678381f687806bef
Aleksandr Straton <houka1337@gmail.com>
2023-07-20 16:31:20 +0300

    added clear screen function

---
+++ src/main/java/org/example/classes/cls.java
@@ -0,0 +1,9 @@
+package org.example.classes;
+
+public class cls
+{
+    public cls() {
+        for(int i = 0; i < 80*300; i++) // Default Height of cmd is 300 and Default width is 80
+            System.out.println("\b"); // Prints a backspace
+    }
+}
